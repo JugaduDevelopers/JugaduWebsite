7e4cf2d1a133c4c7da6b674f9df97a792eeccbc9
Giyu-Tomoika <giyu.tomoika1210@gmail.com>
2023-05-14 15:20:26 +0530

    Done Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,23 @@
 /** @type {import('tailwindcss').Config} */
-const { fontFamily } = require("tailwindcss/defaultTheme");
+const { fontFamily } = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
+  content: ['./src/**/*.{js,jsx}'],
   theme: {
-    colors: {
-      black: "#000000",
-      whiteSmoke: "#F6F6F6",
-      taupeGray: "#8D8C8A",
-      queenBlue: "#408697",
-      turquoise: "#52D6F4",
-    },
-    fontSize: {
-      xs: ["0.75em", "1.125em"],
-      sm: ["0.875em", "1.25em"],
-      base: ["16px", "1.44em"],
-      lg: ["18px", "1.667em"],
-      xl: ["20px", "1.5em"],
-      xxl: ["22px", "1.5em"],
-      "2xl": ["24px", "1.583em"],
-      "3xl": ["26px", "1.289em"],
-      "4xl": ["32px", "1.289em"],
-      "5xl": ["36px", "1.3em"],
-      "6xl": ["38px", "1.289em"],
-      "7xl": ["42px", "1.3em"],
-      "8xl": ["52px", "1.3em"],
-      "9xl": ["60px", "1.3em"],
+    extend: {
+      gridTemplateColumns: {
+        'auto-320px': 'repeat(auto-fit, 20rem)',
+      },
+      spacing: {
+        container: `max(
+            1rem,
+            calc((100vw - calc(1440px - 1rem * 2)) / 2)
+            )`,
+        'sm-container': `max(
+            1rem,
+            calc((100vw - calc(550px - 1rem * 2)) / 2)
+            )`,
+      },
     },
   },
-  plugins: [ require('@tailwindcss/line-clamp'),],
-};
+}
